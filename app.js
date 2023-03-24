774082023c82208e7597ca310fa1d36bd39b4cac
abedassoum <abd.assoum@outlook.com>
2023-03-24 17:02:34 +0100

    up

--- app.js
+++ app.js
@@ -3,10 +3,9 @@
 
 window.addEventListener("load", initApp)
 
-async function  initApp(){
+async function initApp(){
 const pikachu = await getPokemon("pikachu.json")
-showInformation(pikachu);
-// showPokemon(pikachu);
+showPokemon(pikachu);
 
 }
 
@@ -16,65 +15,44 @@ async function getPokemon (url) {
  return data;
 }
 
-
-
-function showInformation(pokemon){
-const myHTML = /*html*/ `
-<li>Name: ${pokemon.name}</li>
-<li>Description: ${pokemon.description}</li>
-<li>Ability: ${pokemon.ability}</li>
-<img src = "${pokemon.image}"/>
-<li>Foorprint: ${pokemon.footprint}</li>
-<li>Dexindex: ${pokemon.dexindex}</li>
-<li>Type: ${pokemon.type}</li>
-<li>subtype: ${pokemon.subtype}</li>
-<li>Weaknesses: ${pokemon.weaknesses}</li>
-<li>Gender: ${pokemon.gender}</li>
-<li>Weight: ${pokemon.weight}</li>
-<li>Height: ${pokemon.height}</li>
-<li>Generation: ${pokemon.generation}</li>
-<li>Spilversion: ${pokemon.spilversion}</li>
-<li>Can Evolve: ${pokemon.canEvolve}</li>
-<li>HP: ${pokemon.statsHP}</li>
-<li>Attack: ${pokemon.statsAttack}</li>
-<li>Special attack: ${pokemon.statsSpecialAttack}</li>
-<li>Special defence: ${pokemon.statsSpecialDefence}</li>
-<li>Speed: ${pokemon.statsSpeed}</li>
-`;
-document.querySelector("#pokemons").insertAdjacentHTML("beforeend", myHTML)
-document.querySelector("#pokemon article:last-child").addEventListener("click", pokemonClicked);
-
-
-}
-
-function pokemonClicked(pokemon) {
+function showPokeModal(pokemon) {
   console.log(pokemon);
-
-  document.querySelector("#dialog_pokemon_firstname").textContent = pokemon.name;
-  document.querySelector("#dialog-type").textContent = pokemon.type;
-  document.querySelector("#dialog-dexindex").textContent = pokemon.dexindex;
-  document.querySelector("#dialog-image").src = pokemon.image;
+  document.querySelector("#dialog-image").src = `${pokemon.image}`
+  document.querySelector("#dialog-name").textContent = `${pokemon.name}`
+  document.querySelector("#dialog-description").textContent = `${pokemon.description}`
+  document.querySelector("#dialog-ability").textContent = `${pokemon.ability}`;
+  document.querySelector("#dialog-dexIndex").textContent = `${pokemon.dexIndex}`;
+  document.querySelector("#dialog-subType").textContent = `${pokemon.subType}`;
+  document.querySelector("#dialog-weaknesses").textContent = `${pokemon.weaknesses}`;
+  document.querySelector("#dialog-gender").textContent = `${pokemon.gender}`;
+  document.querySelector("#dialog-weight").textContent = `${pokemon.weight}`;
+  document.querySelector("#dialog-height").textContent = `${pokemon.height}`;
+  document.querySelector("#dialog-generation").textContent = `${pokemon.generation}`;
+  document.querySelector("#dialog-gameVersion").textContent = `${pokemon.gameVersion}`;
+  document.querySelector("#dialog-evolve").textContent = `${pokemon.canEvolve}`;
+  document.querySelector("#dialog-HP").textContent = `${pokemon.statsHP}`;
+  document.querySelector("#dialog-attack").textContent = `${pokemon.statsAttack}`;
+  document.querySelector("#dialog-defence").textContent = `${pokemon.statsDefence}`;
+  document.querySelector("#dialog-specialAttack").textContent = `${pokemon.statsSpecialAttack}`
+  document.querySelector("#dialog-specialDefence").textContent = `${pokemon.statsSpecialDefence}`
+  document.querySelector("#dialog-speed").textContent = `${pokemon.statsSpeed}`;
   document.querySelector("#dialog").showModal();
-  // show dialog
-  document.querySelector("#dialog-pokemon").showModal();
 }
-
-
 
 function showPokemon(pokemon) {
   console.log(pokemon);
-  document.querySelector("#pokemons").insertAdjacentHTML(
-    "beforeend",
-    `<article class="grid-item">
-       <img src="${pokemon.image}"/>
-       <h2>${pokemon.name}</h2>
-       <p>Type ${pokemon.type}, ${pokemon.subtype}</p>
-       <p>Pokedex Number: ${pokemon.dexindex}</p>
-     </article>`
-     
-  );
-
-  document.querySelector("#pokemons article:last-child").addEventListener("click", pokemonClicked);
+  document.querySelector("#pokemon").insertAdjacentHTML(
+    "beforeend", /*HTML*/`
+      <article>
+        <img src="${pokemon.image}"/>
+        <h2>${pokemon.name}</h2>
+        <p>Type: ${pokemon.type}</p>
+        <p>Pokedex Number: ${pokemon.dexIndex}</p>
+     </article>`);
+  document.querySelector("#pokemon article:last-child").addEventListener("click", pokemonClicked);    
+  function pokemonClicked() {
+   showPokeModal(pokemon);
+  }
 }
 
  
